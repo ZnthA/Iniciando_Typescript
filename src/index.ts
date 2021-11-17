9ca72a644053ad7b5f4912b7d8011c5859000e51
Zinthia <zinthiadesign@gmail.com>
2021-11-17 10:58:20 -0500

    Cambios nuevos

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,3 @@
     ===== Código de TypeScript =====
 */
 
-
-function sumar(a: number, b:number): number{
-    return a + b;
-}
-const sumarFlecha = (a: number,b: number):number => {
-    return a + b
-}
-
-function multiplicar(numero: number,otroNumero?: number,base:number = 2): number{
-    return numero * base;
-}
-
-function curar(personaje, curarX): void {
-    personaje.hp += curarX;
-
-    console.log(personaje);
-}
